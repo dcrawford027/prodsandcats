d073b19db82149fca8730ebbb86af763383b8fd0
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-08-16 21:34:34 -0400

    completed assignment

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProductsAndCategories.Models;
+
+namespace ProductsAndCategories.Controllers
+{
+    public class HomeController : Controller
+    {
+        private PacContext db;
+
+        public HomeController(PacContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return RedirectToAction("Categories");
+        }
+
+        [HttpGet("{categoryId}")]
+        public IActionResult Category(int categoryId)
+        {
+            Category category = db.Categories
+                .Include(cat => cat.Products)
+                .ThenInclude(association => association.Product)
+                .FirstOrDefault(cat => cat.CategoryId == categoryId);
+
+            List<Product> products = db.Products.ToList();
+            ViewBag.Products = products;
+
+            return View("Index", category);
+        }
+
+        [HttpPost("{categoryId}/add_product")]
+        public IActionResult AddProductToCategory(int productId, int categoryId, Association newAssociation)
+        {
+            newAssociation.ProductId = productId;
+            newAssociation.CategoryId = categoryId;
+            db.Associations.Add(newAssociation);
+            db.SaveChanges();
+            return RedirectToAction("Categories");
+        }
+
+        [HttpGet("categories")]
+        public IActionResult Categories()
+        {
+            List<Category> categories = db.Categories.ToList();
+            ViewBag.Categories = categories;
+            return View("Categories");
+        }
+
+        [HttpPost("categories/create")]
+        public IActionResult CreateCategory(Category newCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(newCategory);
+                db.SaveChanges();
+                return RedirectToAction("Categories");
+            }
+            return View("Categories");
+        }
+
+        [HttpGet("products")]
+        public IActionResult Products()
+        {
+            List<Product> products = db.Products.ToList();
+            ViewBag.Products = products;
+            return View("Products");
+        }
+
+        [HttpPost("products/create")]
+        public IActionResult CreateProduct(Product newProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(newProduct);
+                db.SaveChanges();
+                return RedirectToAction("Products");
+            }
+            return View("Products");
+        }
+
+        [HttpGet("products/{productId}")]
+        public IActionResult Product(int productId)
+        {
+            var product = db.Products
+                .Include(prod => prod.Categories)
+                .ThenInclude(association => association.Category)
+                .FirstOrDefault(prod => prod.ProductId == productId);
+
+            List<Category> categories = db.Categories.ToList();
+            ViewBag.Categories = categories;
+
+            return View("Product", product);
+        }
+
+        [HttpPost("products/{productId}/add_category")]
+        public IActionResult AddCategoryToProduct(int productId, int categoryId, Association newAssociation)
+        {
+            newAssociation.ProductId = productId;
+            newAssociation.CategoryId = categoryId;
+            db.Associations.Add(newAssociation);
+            db.SaveChanges();
+            return RedirectToAction("Products");
+        }
+
+    }
+}
---
+++ Models/PacContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductsAndCategories.Models
+{
+    public class PacContext : DbContext
+    {
+        public PacContext(DbContextOptions options) : base(options) { }
+        public DbSet<Product> Products {get;set;}
+        public DbSet<Category> Categories {get;set;}
+        public DbSet<Association> Associations {get;set;}
+    }
+}
